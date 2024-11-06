5609164725e195098857bf09fc8b61ae94bf9359
SAR CORP <abimas654@gmail.com>
2024-11-06 19:23:07 +0500

    GEMINI API Integration

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -45,20 +45,25 @@ const MainContent = () => {
             <p className="text-slate-700 text-lg">
               Why SEO is required for Websites?
             </p>
-            <BsGraphUpArrow className="text-4xl p-1 absolute bottom-2 right-2"/>
+            <BsGraphUpArrow className="text-4xl p-1 absolute bottom-2 right-2" />
           </div>
         </div>
         <div className="absolute bottom-0 w-full max-w-[900px] px-5 mx-auto mt-5">
           <div className="flex items-center justify-between gap-20 bg-gray-200 py-2 px-5 rounded-full">
-            <input type="text" placeholder="Enter a prompt here..." className="flex-1 bg-transparent border-none outline-none  p-2 text-lg"/>
-            <div className="flex gap-4 items-center"> 
-            <MdOutlineAddPhotoAlternate className="text-2xl cursor-pointer"/> 
-            <FaMicrophone className="text-2xl cursor-pointer"/>
-            <IoMdSend className="text-2xl cursor-pointer"/>
+            <input
+              type="text"
+              placeholder="Enter a prompt here..."
+              className="flex-1 bg-transparent border-none outline-none  p-2 text-lg"
+            />
+            <div className="flex gap-4 items-center">
+              <MdOutlineAddPhotoAlternate className="text-2xl cursor-pointer" />
+              <FaMicrophone className="text-2xl cursor-pointer" />
+              <IoMdSend className="text-2xl cursor-pointer" />
             </div>
           </div>
           <p className="text-sm my-4 mx-auto text-center font-[500] text-slate-500">
-            Gemini may display inaccurate info, including ppeople, so double-check its response.
+            Gemini may display inaccurate info, including ppeople, so
+            double-check its response.
           </p>
         </div>
       </div>
